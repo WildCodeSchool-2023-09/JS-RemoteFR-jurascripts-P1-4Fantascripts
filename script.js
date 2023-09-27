c2bb2148df3343bc925571168d89fbd6989b5fe9
Tetardtek <kvnn64@gmail.com>
2023-09-27 11:37:15 +0200

    First stage of project

---
+++ script.js
@@ -0,0 +1,23 @@
+// sélectionner le boutton effacé
+const clear = document.getElementById("clear");
+// sélectionner l'élément avec l'id "date"
+const dateElement = document.getElementById("date");
+// sélectionner la liste
+const list = document.getElementById("list");
+// sélectionner l'input, la saisie
+const input = document.getElementById("input")
+
+
+
+
+
+
+
+
+// TODOLIST
+
+// crée une zone de texte
+// envoyer son texte dans la "todolist" en liste
+// a chaque entrée de liste avoir une fonction pour "valider"(barre le texte) puis "supprimé"(bruit de papier qui se froisse ?) 
+// ou si "supprimé(demande validation)"
+//
