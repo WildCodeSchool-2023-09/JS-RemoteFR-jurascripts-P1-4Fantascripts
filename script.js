42280f2eeed7637d8405acc573b486677b510d12
Dricks45 <144217492+Dricks45@users.noreply.github.com>
2023-10-01 22:06:09 +0200

    ajout: du boutton clear + fonction,de la date dans le footer et des icon sur les todo(circle et trash)

--- script.js
+++ script.js
@@ -15,12 +15,28 @@
 const button = document.querySelector(`.button`);
 const ul = document.querySelector(`.todo`);
 
+clear.addEventListener("click", function(){
+  localStorage.clear();
+  location.reload();
+});
+
+
 button.addEventListener(`click`, (e) => {
   e.preventDefault();
   const input = document.querySelector(`.input`);
-  const newTask = document.createElement(`li`);
-  newTask.classList.add(`newTask`)
+  const newTask = document.createElement('li');
+  newTask.classList.add(`newTask`);
   newTask.innerText = input.value;
+
+  const circle = document.createElement('i');
+  circle.className = 'fa-regular fa-circle';
+
+  const trash = document.createElement('i');
+  trash.className = 'fa-regular fa-trash-can';
+
+  newTask.appendChild(circle);
+  newTask.appendChild(trash);
+
   ul.appendChild(newTask);
   input.value = ``;
 });
@@ -41,3 +57,8 @@ button.addEventListener(`click`, (e) => {
 //   toDoList.appendChild(listDiv);
 //   input.value = "";
 // }
+
+const dateElement = document.getElementById("date");
+const options = {weekday : "long", month:"long", day:"numeric"};
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("fr-FR", options)
