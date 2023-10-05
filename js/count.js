d5c49de60eea9647ea21b109eb4bdcba4c1a9eeb
Tetardtek <kvnn64@gmail.com>
2023-10-05 15:51:05 +0200

    preparing hash/Js

--- js/count.js
+++ js/count.js
@@ -1,5 +1,51 @@
-let achievedCount = 0;
-let standbyCount = 0;
+// MAJ les compteurs dans localStorage
+
+function todoCounter() {
+  const counterAchieved = document.getElementById("counter-achieved");
+  const counterStandby = document.getElementById("counter-standby");
+  const tasks = todoList.querySelectorAll(".task");
+  const clearButton = document.getElementById("clear-button");
+
+  let achievedCount = 0;
+  let standbyCount = 0;
+
+  tasks.forEach((task) => {
+    if (task.classList.contains("achieved")) {
+      achievedCount++;
+    } else {
+      standbyCount++;
+    }
+  });
+
+  localStorage.setItem("achievedCount", achievedCount);
+  localStorage.setItem("standbyCount", standbyCount);
+
+  counterAchieved.textContent = achievedCount;
+  counterStandby.textContent = standbyCount;
+
+
+  // Ajout du bouton Clear pour refresh les compteurs
+
+  clearButton.addEventListener("click", () => {
+    localStorage.setItem("achievedCount", 0);
+    localStorage.setItem("standbyCount", 0);
+
+    counterAchieved.textContent = 0;
+    counterStandby.textContent = 0;
+  });
+}
+
+
+document.addEventListener("contentCounter", () => {
+  const counterAchieved = document.getElementById("counter-achieved");
+  const counterStandby = document.getElementById("counter-standby");
+  const counters = getCounter();
+
+  counterAchieved.textContent = counters.achievedCount;
+  counterStandby.textContent = counters.standbyCount;
+
+});
+
 // pour récupérer les compteurs du localStorage
 
 function getCounter() {
@@ -9,49 +55,5 @@ function getCounter() {
   return { achievedCount: parseInt(achievedCount), standbyCount: parseInt(standbyCount) };
 }
 
-// MAJ les compteurs dans localStorage
 
-function todoCounter() {
-    const counterAchieved = document.getElementById("counter-achieved");
-    const counterStandby = document.getElementById("counter-standby");
-    const tasks = todoList.querySelectorAll(".task");
-    const clearButton = document.getElementById("clear-button");
-  
-  
-    tasks.forEach((task) => {
-      if (task.classList.contains("achieved")) {
-        achievedCount++;
-      } else {
-        standbyCount++;
-      }
-    });
-  
-    localStorage.setItem("achievedCount", achievedCount);
-    localStorage.setItem("standbyCount", standbyCount);
-  
-    counterAchieved.textContent = achievedCount;
-    counterStandby.textContent = standbyCount;
-  
-  
-    // Ajout du bouton Clear pour refresh les compteurs
-  
-    clearButton.addEventListener("click", () => {
-      localStorage.setItem("achievedCount", 0);
-      localStorage.setItem("standbyCount", 0);
-  
-      counterAchieved.textContent = 0;
-      counterStandby.textContent = 0;
-    });
-  }
-  
-  
-  document.addEventListener("contentCounter", () => {
-    const counterAchieved = document.getElementById("counter-achieved");
-    const counterStandby = document.getElementById("counter-standby");
-    const counters = getCounter();
-  
-    counterAchieved.textContent = counters.achievedCount;
-    counterStandby.textContent = counters.standbyCount;
-  
-  });
-  
+export {todoCounter, getCounter};
--- js/todo.js
+++ js/todo.js
@@ -1,3 +1,5 @@
+// import {todoCounter, getCounter} from "/count.js"
+
 // TODO TASK MANAGER
 
 // SELECTEURS
@@ -84,63 +86,6 @@ function filterCategory(e) {
   });
 }
 
-// // MAJ les compteurs dans localStorage
-
-// function todoCounter() {
-//   const counterAchieved = document.getElementById("counter-achieved");
-//   const counterStandby = document.getElementById("counter-standby");
-//   const tasks = todoList.querySelectorAll(".task");
-//   const clearButton = document.getElementById("clear-button");
-
-//   let achievedCount = 0;
-//   let standbyCount = 0;
-
-//   tasks.forEach((task) => {
-//     if (task.classList.contains("achieved")) {
-//       achievedCount++;
-//     } else {
-//       standbyCount++;
-//     }
-//   });
-
-//   localStorage.setItem("achievedCount", achievedCount);
-//   localStorage.setItem("standbyCount", standbyCount);
-
-//   counterAchieved.textContent = achievedCount;
-//   counterStandby.textContent = standbyCount;
-
-
-//   // Ajout du bouton Clear pour refresh les compteurs
-
-//   clearButton.addEventListener("click", () => {
-//     localStorage.setItem("achievedCount", 0);
-//     localStorage.setItem("standbyCount", 0);
-
-//     counterAchieved.textContent = 0;
-//     counterStandby.textContent = 0;
-//   });
-// }
-
-
-// document.addEventListener("contentCounter", () => {
-//   const counterAchieved = document.getElementById("counter-achieved");
-//   const counterStandby = document.getElementById("counter-standby");
-//   const counters = getCounter();
-
-//   counterAchieved.textContent = counters.achievedCount;
-//   counterStandby.textContent = counters.standbyCount;
-
-// });
-
-// // pour récupérer les compteurs du localStorage
-
-// function getCounter() {
-
-//   const achievedCount = localStorage.getItem("achievedCount") || 0;
-//   const standbyCount = localStorage.getItem("standbyCount") || 0;
-//   return { achievedCount: parseInt(achievedCount), standbyCount: parseInt(standbyCount) };
-// }
-
 
 
 
@@ -166,3 +111,60 @@ function filterCategory(e) {
 //     --color-hover : #6f4c37;
 //     --background-hover : #dacbcb;
 //     --primary-transition: all 0.5s ease;
+
+// MAJ les compteurs dans localStorage
+
+function todoCounter() {
+  const counterAchieved = document.getElementById("counter-achieved");
+  const counterStandby = document.getElementById("counter-standby");
+  const tasks = todoList.querySelectorAll(".task");
+  const clearButton = document.getElementById("clear-button");
+
+  let achievedCount = 0;
+  let standbyCount = 0;
+
+  tasks.forEach((task) => {
+    if (task.classList.contains("achieved")) {
+      achievedCount++;
+    } else {
+      standbyCount++;
+    }
+  });
+
+  localStorage.setItem("achievedCount", achievedCount);
+  localStorage.setItem("standbyCount", standbyCount);
+
+  counterAchieved.textContent = achievedCount;
+  counterStandby.textContent = standbyCount;
+
+
+  // Ajout du bouton Clear pour refresh les compteurs
+
+  clearButton.addEventListener("click", () => {
+    localStorage.setItem("achievedCount", 0);
+    localStorage.setItem("standbyCount", 0);
+
+    counterAchieved.textContent = 0;
+    counterStandby.textContent = 0;
+  });
+}
+
+
+document.addEventListener("contentCounter", () => {
+  const counterAchieved = document.getElementById("counter-achieved");
+  const counterStandby = document.getElementById("counter-standby");
+  const counters = getCounter();
+
+  counterAchieved.textContent = counters.achievedCount;
+  counterStandby.textContent = counters.standbyCount;
+
+});
+
+// pour récupérer les compteurs du localStorage
+
+function getCounter() {
+
+  const achievedCount = localStorage.getItem("achievedCount") || 0;
+  const standbyCount = localStorage.getItem("standbyCount") || 0;
+  return { achievedCount: parseInt(achievedCount), standbyCount: parseInt(standbyCount) };
+}
